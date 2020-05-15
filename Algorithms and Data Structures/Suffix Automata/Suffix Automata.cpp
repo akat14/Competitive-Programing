d89c7134b4267c45acf22ce6ee883472fa29a46c
akat14 <akata14@gmail.com>
2020-05-15 11:44:23 +0300

    more dynamic

--- Algorithms and Data Structures/Suffix Automata/Suffix Automata.cpp
+++ Algorithms and Data Structures/Suffix Automata/Suffix Automata.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 struct SuffixAutomata
 {
-	static const int N=;
-	unordered_map<char,int>adj[N];
-	int len[N],fail[N];
+	vector<unordered_map<char,int>>adj;
+	vector<int>len,fail,matches,cntf;
 	int n,last;
 	SuffixAutomata(int max)
 	{
 		n=1;
 		last=0;
-		for(int i=0;i<2*max;i++)
-			len[i]=fail[i]=0;
+		len=fail=matches=cntf=used=vector<int>(2*max+1,0);
+		adj=vector<unordered_map<char,int>>(2*max+1);
 	}
 	void add_char(char c)
 	{
 		int cur=n++,p,q;
 		len[cur]=len[last]+1;
+		matches[cur]=1;
 		p=last;
 		while(!adj[p][c])
 		{
@@ -29,21 +29,38 @@ struct SuffixAutomata
 		else
 		{
 			int copy=n++;
+			matches[copy]=0;
 			adj[copy]=adj[q];
 			len[copy]=len[p]+1;
 			fail[copy]=fail[q];
 			fail[cur]=fail[q]=copy;
+			cntf[copy]++;
 			while(adj[p][c]==q)
 			{
 				adj[p][c]=copy;
 				p=fail[p];
 			}
 		}
+		cntf[fail[cur]]++;
 		last=cur;
 	}
 	void build(string &s)
 	{
 		for(char c:s)
 			add_char(c);
+	}
+	void find_matches()
+	{
+		queue<int>q;
+		for(int i=0;i<n;i++)
+			if(!cntf[i])q.push(i);
+		while(q.size())
+		{
+			int x=q.front();
+			q.pop();
+			matches[fail[x]]+=matches[x];
+			cntf[fail[x]]--;
+			if(!cntf[fail[x]])q.push(fail[x]);
+		}
 	}
 };
