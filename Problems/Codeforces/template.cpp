c08d16287216d4fc284fe087fd0a1c269df36771
Valentin Borisov <akata14@gmail.com>
2025-11-12 19:32:02 +0200

    CF template

---
+++ Problems/Codeforces/template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+void read(vector<T> &v)
+{
+  for(T &x : v)
+    cin >> x;
+}
+template <typename T>
+void print(vector<T> &v)
+{
+  for(T &x : v)
+    cout << x << ' ';
+  cout << '\n';
+}
+void solve() {}
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
+  cin >> t;
+  while(t--)
+  {
+    solve();
+  }
+}
